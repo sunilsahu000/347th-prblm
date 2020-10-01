a5a33315ffbde6da1dc9b94225bbc6f2449ee771
sunilsahu000 <72176852+sunilsahu000@users.noreply.github.com>
2020-10-01 10:11:34 +0530

    Update expression if and when.kt
    The when expression also check the ranges of input provided in when condition. A range is created using .. (double dot) operator. The in operator is used to check if a value belongs to a range.
    Thank you sir...

--- expression if and when.kt
+++ expression if and when.kt
@@ -5,12 +5,22 @@ fun main(args:Array<String>)
     var max = if(n1>n2) n1 else n2
     print("Max : $max")
 
-    var age=30
 
-    var isYoung= when(age)
-    {
-        30->true
-        else->false
+    var number = 19
+    var age = when(number) {
+       in 1..10 ->println("child")
+        in 10..20 ->println("school life")
+       in 20..30 ->println("struggle life")
+       in 30..40 ->println("Enjoy your life")
+        
+        else ->println("incorrect nomber")
     }
-    print("Is Youg : $isYoung")
+  
 }
+
+   
+   
+  
+
+    
+   
